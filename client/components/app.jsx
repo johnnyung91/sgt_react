7598b0c91185ba60489672d794b4a3965a1ab3ce
Johnny Ung <johnnyung91@gmail.com>
2020-04-18 06:05:56 +0000

    created constructor method on App component; set initial state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [] };
+  }
+
   render() {
     return null;
   }
