421106ac6be6a6440fccd6debb8210d2a9c3bd26
Johnny Ung <johnnyung91@gmail.com>
2020-04-19 08:28:55 +0000

    feature 3 - user can add grade completed; look into refactoring and adding form validation

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -12,6 +12,7 @@ class GradeForm extends React.Component {
     this.handleCourse = this.handleCourse.bind(this);
     this.handleGrade = this.handleGrade.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleName(event) {
@@ -40,28 +41,38 @@ class GradeForm extends React.Component {
       grade: this.state.grade
     };
     this.props.onSubmit(newGrade);
-    this.setState = {
+    this.setState({
       name: '',
       course: '',
       grade: ''
-    };
-    event.target.reset();
+    });
+  }
+
+  handleReset(event) {
+    event.preventDefault();
+    this.setState({
+      name: '',
+      course: '',
+      grade: ''
+    });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleAdd}>
+      <form onSubmit={this.handleAdd} onReset={this.handleReset}>
         <h3 className="mb-2">Add Student</h3>
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text"><i className="fa fa-user"></i></span>
           </div>
           <input
+            required
             type="text"
             className="form-control"
             id="student-name-course"
             placeholder="Student Course"
             name="course"
+            value={this.state.name}
             onChange={this.handleName}
           />
         </div>
@@ -70,11 +81,13 @@ class GradeForm extends React.Component {
             <span className="input-group-text"><i className="fa fa-list"></i></span>
           </div>
           <input
+            required
             type="text"
             className="form-control"
             id="student-name-course"
             placeholder="Student Course"
             name="course"
+            value={this.state.course}
             onChange={this.handleCourse}
           />
         </div>
@@ -83,17 +96,19 @@ class GradeForm extends React.Component {
             <span className="input-group-text"><i className="fa fa-graduation-cap"></i></span>
           </div>
           <input
+            required
             type="text"
             className="form-control"
             id="student-name-grade"
             placeholder="Student Grade"
             name="grade"
+            value={this.state.grade}
             onChange={this.handleGrade}
           />
         </div>
         <div className="input-buttons">
           <input className="btn btn-success mr-2" type="submit" value="Add" />
-          <input className="btn btn-outline-secondary" type="reset" value="Cancel" />
+          <input className="btn btn-outline-secondary" type="reset" value="Cancel"/>
         </div>
       </form>
     );
